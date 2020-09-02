c752c04927692e9ccaabad3655dab1981b85da31
kudaikulov <70584169+kudaikulov@users.noreply.github.com>
2020-09-02 12:38:01 +0600

    Initial commit

---
+++ app/src/main/java/com/example/kolesateamlesson2/Hello/HelloActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kolesateamlesson2.Hello
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kolesateamlesson2.R
+
+class HelloActivity: AppCompatActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello)
+    }
+}
